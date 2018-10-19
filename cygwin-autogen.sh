4310789b638816d07dd91997a7e66787e7031566
Craig Comstock <craig.comstock@northern.tech>
2018-10-19 21:46:37 +0000

    wip

--- cygwin-autogen.sh
+++ cygwin-autogen.sh
@@ -1,6 +1,9 @@
 #CC=x86_64-w64-mingw32-gcc \
-CPPFLAGS=" -mwin32 " \
-CFLAGS=" -mwin32 " \
+#CPPFLAGS=" -mwin32 " \
+#CFLAGS=" -mwin32 " \
+CPPFLAGS=" -mwin32 -I/mingw64/include " \
+CFLAGS=" -mwin32 -I/mingw64/include " \
+LDFLAGS=" -L/mingw64/lib " \
 ./autogen.sh \
 --without-pam
 #CPPFLAGS=" -pthread " \
