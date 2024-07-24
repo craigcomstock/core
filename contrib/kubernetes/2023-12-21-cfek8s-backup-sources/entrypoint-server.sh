385eac9482f0f0d9313f20e025b958f36ca91e06
Craig Comstock <craig.comstock@northern.tech>
2024-07-23 22:27:29 -0500

    Craigs-MacBook-Pro.local

---
+++ contrib/kubernetes/2023-12-21-cfek8s-backup-sources/entrypoint-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+export CALLED_FROM_INITSCRIPT=1
+source /var/cfengine/bin/cfengine3-nova-hub-init-d.sh
+start_postgres
+start_httpd
+
+CFENGINE_HUB_IP=$(kubectl describe pod -l app=cfengine-hub | grep ^IP: | awk {'print $2'})
+/var/cfengine/bin/cf-agent --bootstrap ${CFENGINE_HUB_IP} --log-level info
+
+# TODO: to manage this properly we probably need to run the daemons in separate containers within a pod for the livecycle management by Kubernetes
+for process in cf-execd cf-monitord cf-serverd cf-hub cf-reactor; do
+    echo "${process} is not running. Starting..."
+    /var/cfengine/bin/${process}
+done
+while true; do sleep 3600; done
